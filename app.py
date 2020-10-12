3e6228c9833dbd64f7b02e49186761c1c3d0c0d8
ShristiJalan <shristijalan1010@gmail.com>
2020-10-12 11:56:10 +0530

    static search successful
    created a form for text and a button

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+import wikipedia
+
 app = Flask(__name__)
+
+x='tiger'
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/result')
+def result():
+    return wikipedia.summary(x)
     
 if __name__ == '__main__':
     app.run(debug = True)
